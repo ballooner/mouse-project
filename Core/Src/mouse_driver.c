27d7dc194e90c549ff83d33888677db4814eae4e
Jace-Desktop <26182964+ballooner@users.noreply.github.com>
2026-08-11 05:54:20 -0400

    Added USB device descriptor and removed UART test statements

--- Core/Src/mouse_driver.c
+++ Core/Src/mouse_driver.c
@@ -11,6 +11,24 @@
 
 extern UART_HandleTypeDef huart2;
 
+static const uint8_t device_descriptor[] =
+{
+		0x12,			//bLength
+		0x01,			//bDescriptorType
+		0x02, 0x00,		//bcdUSB
+		0x00,			//bDeviceClass
+		0x00,			//bDeviceSubClass
+		0x00,			//bDeviceProtocol
+		0x40,			//bMaxPacketSize
+		0x83, 0x04,		//idVendor
+		0x01, 0x00,		//idProduct
+		0x00, 0x01,		//bcdDevice
+		0x00,			//iManufacturer
+		0x00,			//iProduct
+		0x00,			//iSerialNumber
+		0x01			//bNumConfigurations
+};
+
 HAL_StatusTypeDef USB_Start(PCD_HandleTypeDef *hpcd_USB) {
 	HAL_PCD_Start(hpcd_USB);
 
@@ -18,27 +36,36 @@ HAL_StatusTypeDef USB_Start(PCD_HandleTypeDef *hpcd_USB) {
 }
 
 void HAL_PCD_SetupStageCallback(PCD_HandleTypeDef *hpcd) {
+	USB_OTG_GlobalTypeDef USBx = hpcd->Instance;
 	uint8_t *setup = (uint8_t*) hpcd->Setup;
 
-	//uint8_t bmRequestType = setup[0];
+	uint8_t bmRequestType = setup[0];
 	uint8_t bRequest = setup[1];
-	//uint16_t wValue = setup[2] | ((uint16_t) setup[3 << 8]);
-	//uint16_t wIndex = setup[4] | ((uint16_t) setup[5] << 8);
-	//uint16_t wLength = setup[5] | ((uint16_t) setup[6] << 8);
+	uint16_t wValue = setup[2] | ((uint16_t) setup[3] << 8);
+	uint16_t wIndex = setup[4] | ((uint16_t) setup[5] << 8);
+	uint16_t wLength = setup[5] | ((uint16_t) setup[6] << 8);
 
 	unsigned char uart_message[] = "Request identified\r\n";
 
 	switch (bRequest) {
 	case BREQUEST_GET_STATUS:
+		// Device is self-powered and doesn't need to wake the host up during suspend
+		uint16_t response = 0x00;
+		USB_WritePacket(USBx, &response, 0, 2, 0);
+		break;
 	case BREQUEST_CLEAR_FEATURE:
+		break;
 	case BREQUEST_SET_FEATURE:
+		break;
 	case BREQUEST_SET_ADDRESS:
+		break;
 	case BREQUEST_GET_DESCRIPTOR:
+		break;
 	case BREQUEST_SET_DESCRIPTOR:
+		break;
 	case BREQUEST_GET_CONFIGURATION:
+		break;
 	case BREQUEST_SET_CONFIGURATION:
-		HAL_UART_Transmit(&huart2, (uint8_t*) uart_message,
-					sizeof(uart_message) - 1, 100);
 		break;
 	default:
 	}
