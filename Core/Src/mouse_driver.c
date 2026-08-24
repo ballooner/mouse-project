fc6e2a72ec955bf19af642dc9431ba336798af6a
ballooner <26182964+ballooner@users.noreply.github.com>
2026-08-24 18:17:41 -0400

    Finished device and config descriptors

--- Core/Src/mouse_driver.c
+++ Core/Src/mouse_driver.c
@@ -11,7 +11,7 @@
 
 extern UART_HandleTypeDef huart2;
 
-static const uint8_t device_descriptor[] =
+static const uint8_t	device_descriptor[] =
 {
 		0x12,			//bLength
 		0x01,			//bDescriptorType
@@ -27,6 +27,47 @@ static const uint8_t device_descriptor[] =
 		0x00,			//iProduct
 		0x00,			//iSerialNumber
 		0x01			//bNumConfigurations
+};
+
+static const uint8_t	config_descriptor[] =
+{
+		/* CONFIG DESCRIPTOR */
+		0x09,			//bLength
+		0x02,			//bDescriptorType
+		0x22, 0x00,		//bTotalLength (return 34 bytes of data)
+		0x01,			//bNumInterfaces
+		0x01,			//bConfigurationValue
+		0x00,			//iConfiguration
+		0xC0,			//bmAttributes (Self powered)
+		0x32,			//bMaxPower
+
+		/* INTERFACE DESCRIPTOR */
+		0x09,			//bLength
+		0x04,			//bDescriptorType
+		0x00,			//bInterfaceNumber
+		0x00,			//bAlternateSetting
+		0x01,			//bNumEndpoints (only using 1 interrupt endpoint)
+		0x03,			//bInterfaceClass (HID class)
+		0x01,			//bInterfaceSubClass
+		0x02,			//bInterfaceProtocol
+		0x00,			//iInterface
+
+		/* HID DESCRIPTOR */
+		0x09, 			//bLength
+		0x21,			//bDescriptorType
+		0x10, 0x01,		//bcdHID
+		0x00,			//bCountryCode
+		0x01,			//bNumDescriptors
+		0x22,			//bDescriptorType
+		0x2f, 0x00,		//wDescriptorLength
+
+		/* INTERRUPT IN ENDPOINT DESCRIPTOR */
+		0x07,			//bLength
+		0x05,			//bDescriptorType
+		0x81,			//bEndpointAddress (Endpoint number 1, Direction is IN)
+		0x03,			//bmAttributes (Interrupt type endpoint)
+		0x04, 0x00,		//wMaxPacketSize (Max packet size of 4 bytes)
+		0x01,			//bInterval (Poll at 1ms)
 };
 
 HAL_StatusTypeDef USB_Start(PCD_HandleTypeDef *hpcd_USB) {
